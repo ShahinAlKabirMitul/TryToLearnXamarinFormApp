fcd04056d3c8d39ffef93404fceb2fed7171a15e
ShahinAlKabirMitul <mitul.cse@gmail.com>
2017-11-24 10:50:05 +0600

    Dealing with Device Differences Code behind

--- TryToLearnXamarinFormApp/TryToLearnXamarinFormApp/Geeting.xaml.cs
+++ TryToLearnXamarinFormApp/TryToLearnXamarinFormApp/Geeting.xaml.cs
@@ -16,6 +16,38 @@ namespace TryToLearnXamarinFormApp
         {
             InitializeComponent();
             Slider.Value = 0.5;
+            //if (Device.RuntimePlatform==Device.iOS)
+            //{
+            //    Padding=new Thickness(0,20,0,0);
+            //}
+            //else if (Device.RuntimePlatform==Device.Android)
+            //{
+            //    Padding = new Thickness(10, 20, 0, 0);
+            //}
+            //else if (Device.RuntimePlatform == Device.WinPhone)
+            //{
+            //    Padding = new Thickness(30, 20, 0, 0);
+            //}
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+                case Device.Android:
+                    Padding = new Thickness(10, 20, 10, 5);
+                    break;
+                case Device.WinPhone:
+                    Padding = new Thickness(30, 20, 0, 0);
+                    break;
+                case Device.UWP:
+                    Padding = new Thickness(10, 20, 10, 5);
+                    break;
+                default:
+                    Padding = new Thickness(30, 20, 0, 0);
+                    break;
+            }
+
         }
 
 
