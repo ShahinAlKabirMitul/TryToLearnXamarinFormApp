f23ca23d85ac16d58f04671b783573aba37fc46d
ShahinAlKabirMitul <mitul.cse@gmail.com>
2017-11-24 12:38:41 +0600

    StackLayout Xaml

--- TryToLearnXamarinFormApp/TryToLearnXamarinFormApp/App.xaml.cs
+++ TryToLearnXamarinFormApp/TryToLearnXamarinFormApp/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using TryToLearnXamarinFormApp.Layout;
 using Xamarin.Forms;
 
 namespace TryToLearnXamarinFormApp
@@ -13,7 +13,7 @@ namespace TryToLearnXamarinFormApp
         {
             InitializeComponent();
 
-            MainPage = new QuotesPage();
+            MainPage = new Stack();
         }
 
         protected override void OnStart()
