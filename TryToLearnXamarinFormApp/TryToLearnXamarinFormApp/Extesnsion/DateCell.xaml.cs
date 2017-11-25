e72d61768bc53f17d611904048c660af48798ce5
ShahinAlKabirMitul <mitul.cse@gmail.com>
2017-11-25 23:34:18 +0600

    Bindable Properties

---
+++ TryToLearnXamarinFormApp/TryToLearnXamarinFormApp/Extesnsion/DateCell.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TryToLearnXamarinFormApp.Extesnsion
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DateCell : ViewCell
+	{
+	    private static BindableProperty LabelProperty = BindableProperty.Create("Label", typeof(string), typeof(DateCell));
+
+	    public string Label
+	    {
+	        get { return (string) GetValue(LabelProperty); }
+            set {SetValue(LabelProperty,value); }
+            
+	    }
+		public DateCell ()
+		{
+		    BindingContext = this;
+			InitializeComponent ();
+		}
+	}
+}
