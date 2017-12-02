a328f1656bae946484157b1aaa002bd2a66d8f47
ShahinAlKabirMitul <mitul.cse@gmail.com>
2017-12-02 21:44:11 +0600

    Remove sqlite from all projects

---
+++ TryToLearnXamarinFormApp/TryToLearnXamarinFormApp.Android/SQLiteDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using TryToLearnXamarinFormApp.Droid;
+[assembly: Dependency(typeof(SQLiteDb))]
+namespace TryToLearnXamarinFormApp.Droid
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
